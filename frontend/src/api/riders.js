ca165d607380165854a4b3159d3ce94d01f29385
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:35:00 +0100

    Update riders.js

--- frontend/src/api/riders.js
+++ frontend/src/api/riders.js
@@ -1,5 +1,5 @@
 import apiClient from '../utils/apiClient';
 
-export const joinCarpool = carpoolId => apiClient.post(`/riders/join/${carpoolId}`);
+export const joinCarpool = id => apiClient.post(`/riders/join/${id}`);
 export const autoMatch = eventId => apiClient.post(`/riders/automatch/${eventId}`);
 export const getCoRiders = eventId => apiClient.get(`/riders/coriders/${eventId}`);
