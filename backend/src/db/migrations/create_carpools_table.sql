0ddc5706b845959aa73df4054474a1938afed9b1
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:16:23 +0100

    Update create_carpools_table.sql

--- backend/src/db/migrations/create_carpools_table.sql
+++ backend/src/db/migrations/create_carpools_table.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE carpools (
+  id SERIAL PRIMARY KEY,
+  event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+  driver_id INTEGER NOT NULL REFERENCES drivers(id) ON DELETE CASCADE,
+  meet_point VARCHAR(20) NOT NULL,
+  pickup_time TIME NOT NULL,
+  greggs_enabled BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  UNIQUE(event_id, driver_id)
+);
