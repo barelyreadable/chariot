0a8fcaa2abd452089c4ec35fa1cb7272058b3dcb
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 04:59:04 +0100

    Update create_events_table.sql

--- backend/src/db/migrations/create_events_table.sql
+++ backend/src/db/migrations/create_events_table.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL,
+  meet_point VARCHAR(20) NOT NULL CHECK (meet_point IN ('Clubhouse','Door Pickup','Other')),
+  pickup_time TIME NOT NULL,
+  greggs_pickup BOOLEAN NOT NULL DEFAULT FALSE
+);
