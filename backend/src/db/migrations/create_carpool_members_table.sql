649e6a7b4b05cb937bcf3589a9f7ba7e1d24b1b5
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:16:38 +0100

    Update create_carpool_members_table.sql

--- backend/src/db/migrations/create_carpool_members_table.sql
+++ backend/src/db/migrations/create_carpool_members_table.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE carpool_members (
+  id SERIAL PRIMARY KEY,
+  carpool_id INTEGER NOT NULL REFERENCES carpools(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  UNIQUE(carpool_id, user_id)
+);
