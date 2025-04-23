ac3737a76cab59de453a6817067eba82ab87e981
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 04:58:18 +0100

    Update create_users_table.sql

--- backend/src/db/migrations/create_users_table.sql
+++ backend/src/db/migrations/create_users_table.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(20) NOT NULL CHECK (role IN ('admin','driver','rider'))
+);
