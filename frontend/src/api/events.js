247b6d35d76128aa19e1f2e4763b5c17edb45f02
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:34:32 +0100

    Update events.js

--- frontend/src/api/events.js
+++ frontend/src/api/events.js
@@ -1,6 +1,6 @@
 import apiClient from '../utils/apiClient';
 
-export const createEvent = data => apiClient.post('/events', data);
-export const updateEvent = (id, data) => apiClient.put(`/events/${id}`, data);
 export const getEvents = () => apiClient.get('/events');
+export const createEvent = d => apiClient.post('/events', d);
+export const updateEvent = (id,d) => apiClient.put(`/events/${id}`, d);
 export const deleteEvent = id => apiClient.delete(`/events/${id}`);
