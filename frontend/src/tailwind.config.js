538ebe55b8ea601df4cd7866e671c872bd0b9914
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 05:52:16 +0100

    Update tailwind.config.js

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#310981',
+        secondary: '#f4b55e',
+        accent: '#D69E2E'
+      },
+    },
+  },
+  plugins: [],
+};
