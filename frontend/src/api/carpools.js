6d0349ad14be012055dd40236cb6728dcb3bb5d9
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:34:45 +0100

    Update carpools.js

--- frontend/src/api/carpools.js
+++ frontend/src/api/carpools.js
@@ -4,3 +4,5 @@ export const getCarpools = eventId => apiClient.get(`/carpools/${eventId}`);
 export const getMembers = carpoolId => apiClient.get(`/carpools/${carpoolId}/members`);
 export const toggleGreggs = (carpoolId, enabled) =>
   apiClient.patch(`/carpools/${carpoolId}/greggs-toggle`, { enabled });
+export const subscribeEvent = (eventId, payload) =>
+  apiClient.post(`/carpools/${eventId}/subscribe`, payload);
