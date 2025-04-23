1e432be0f3f38007252ad6e55d3f7c5e9f7bfaa7
barelyreadable <charlie@bytebuddy.uk>
2025-04-23 06:15:23 +0100

    Update create_users_table.sql

--- backend/src/db/migrations/create_users_table.sql
+++ backend/src/db/migrations/create_users_table.sql
@@ -2,5 +2,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  role VARCHAR(20) NOT NULL CHECK (role IN ('admin','driver','rider'))
+  role VARCHAR(20) NOT NULL CHECK (role IN ('admin','driver','rider')),
+  profile_picture VARCHAR(512),
+  greggs_pref VARCHAR(50) DEFAULT 'None',
+  drink_pref VARCHAR(50) DEFAULT 'None'
 );
